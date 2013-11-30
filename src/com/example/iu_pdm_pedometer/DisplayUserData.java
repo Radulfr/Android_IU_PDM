13aa077dfdf91002ad2aa0d3519e31d17df42eb9
Radulfr <raul.elfriky@gmail.com>
2013-11-30 17:54:56 +0100

    initial commit

---
+++ src/com/example/iu_pdm_pedometer/DisplayUserData.java
@@ -0,0 +1,99 @@
+package com.example.iu_pdm_pedometer;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class DisplayUserData extends Activity {
+
+	@SuppressLint("NewApi")
+
+	private TextView tvDisplayTime;
+	static final int TIME_DIALOG_ID = 999;
+	@SuppressLint("NewApi")
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_display_user_data);
+		
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+			getActionBar().setDisplayHomeAsUpEnabled(true); 
+		}
+		
+		Intent i = getIntent();
+
+		float[] data = i.getFloatArrayExtra(MainActivity.THE_DATA);
+		float imc = data[MainActivity.WEIGHT_INDEX]/((data[MainActivity.HEIGHT_INDEX]/100)*(data[MainActivity.HEIGHT_INDEX]/100)); 
+		String message="";
+		short time_training = 30; 
+		if (imc < 18.5)
+			message = "under your better weight";
+		else if( imc >= 18.5 && imc < 25)
+			message = "in your correct weight"; 
+		else if( imc >= 25 && imc < 30){
+			message = "over your  better weight";
+			time_training = 40;
+		}
+		else if (imc >= 30){
+			message = "obese. Careful!";
+			time_training = 50; 
+		}	
+		TextView tv = (TextView) findViewById(R.id.info); 
+		tv.setText("Your IMC is " + imc +
+				"\n\nYou are probably "+message +
+				"\n\nTraining time estimated: " + time_training +
+				"\nCan you select your top time?"); 
+		//setContentView(tv); 
+		// Show the Up button in the action bar.
+		setupActionBar();
+	}
+	public void saveNGo(View view){
+		TimePicker tp = (TimePicker) findViewById(R.id.timePicker);
+		int hour = tp.getCurrentHour();
+		int minute = tp.getCurrentMinute();
+		System.out.println("theTime = " +hour+":"+minute);
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.display_user_data, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
---
+++ src/com/example/iu_pdm_pedometer/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.iu_pdm_pedometer;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+	public final static String THE_DATA = "com.example.iu_pdm_pedometer.USERDATA";
+	public final static int HEIGHT_INDEX = 0;
+	public final static int WEIGHT_INDEX = 1;
+	public final static int LEGSIZE_INDEX = 2;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void sendData(View view){
+		Intent i = new Intent(this, DisplayUserData.class); 
+		EditText height = (EditText) findViewById(R.id.Height);
+		EditText weight = (EditText) findViewById(R.id.Weight);
+		EditText leg = (EditText) findViewById(R.id.Leglongitude);
+		
+		float [] values = new float[3];
+		
+		Float h = Float.parseFloat(height.getText().toString());
+		Float w = Float.parseFloat(weight.getText().toString());
+		Float l = Float.parseFloat(leg.getText().toString());
+		
+		System.out.println(h + " - " + w + " " + l);
+		
+		values[HEIGHT_INDEX] = h; 
+		values[WEIGHT_INDEX] = w;
+		values[LEGSIZE_INDEX] = l;
+		i.putExtra(THE_DATA, values);
+		startActivity(i);
+	}
+}
